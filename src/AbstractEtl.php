a5908867c8f89d711e53198e095a24360236bf8c
ivanmarchenko83 <38218499+ivanmarchenko83@users.noreply.github.com>
2020-10-15 11:05:47 -0400

    added statistics info for etl (#3)

--- src/AbstractEtl.php
+++ src/AbstractEtl.php
@@ -7,6 +7,9 @@ abstract class AbstractEtl
     abstract public function extractor(): AbstractExtractor;
     abstract public function loader(): AbstractLoader;
 
+    protected $statistics = [
+    ];
+
     /**
      * @param array $data
      * @return array|Iterator|null
@@ -14,5 +17,10 @@ abstract class AbstractEtl
     public function transform(array $data)
     {
         return $data;
+    }
+
+    public function getStatistics()
+    {
+        return $this->statistics;
     }
 }
